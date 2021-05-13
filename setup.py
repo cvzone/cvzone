c78036e4a331eb564b082eb77e60f2d7cf737c19
murtazahassan <mhpr@live.co.uk>
2021-05-13 23:00:28 +0300

    Added pyserial to install require

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='cvzone',
     packages=['cvzone'],
-    version='1',
+    version='1.1.1',
     license='MIT',
     description='Computer Vision Helping Library',
     author='Computer Vision Zone',
@@ -14,6 +14,7 @@ setup(
         'opencv-python',
         'mediapipe',
         'numpy',
+        'pyserial'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
