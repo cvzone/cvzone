2e7c093d913c8aa23586e2757fb5aad599285745
murtazahassan <mhpr@live.co.uk>
2021-07-31 18:16:39 +0300

    Arduino Auto Detection

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='cvzone',
     packages=['cvzone'],
-    version='1.3.7',
+    version='1.3.8',
     license='MIT',
     description='Computer Vision Helping Library',
     author='Computer Vision Zone',
