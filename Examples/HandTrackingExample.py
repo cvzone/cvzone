db72a4cfe07a306233021fae177f94dd69fc9656
murtazahassan <mhpr@live.co.uk>
2021-09-01 20:03:07 +0300

    HandTracking Example Updated

--- Examples/HandTrackingExample.py
+++ Examples/HandTrackingExample.py
@@ -2,35 +2,37 @@ from cvzone.HandTrackingModule import HandDetector
 import cv2
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
-detector = HandDetector(detectionCon=0.5, maxHands=1)
-
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 while True:
     # Get image frame
     success, img = cap.read()
-
     # Find the hand and its landmarks
-    img = detector.findHands(img)
-    lmList, bboxInfo = detector.findPosition(img)
-    if lmList:
-        bbox = bboxInfo['bbox']
-        # Find how many fingers are up
-        fingers = detector.fingersUp()
-        totalFingers = fingers.count(1)
-        cv2.putText(img, f'Fingers:{totalFingers}', (bbox[0] + 200, bbox[1] - 30),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+    hands, img = detector.findHands(img)  # with draw
+    # hands = detector.findHands(img, draw=False)  # without draw
 
-        # Find Distance Between Two Fingers
-        distance, img, info = detector.findDistance(8, 12, img)
-        cv2.putText(img, f'Dist:{int(distance)}', (bbox[0] + 400, bbox[1] - 30),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+    if hands:
+        # Hand 1
+        hand1 = hands[0]
+        lmList1 = hand1["lmList"]  # List of 21 Landmark points
+        bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
+        centerPoint1 = hand1['center']  # center of the hand cx,cy
+        handType1 = hand1["type"]  # Handtype Left or Right
 
-        # Find Hand Type
-        myHandType = detector.handType()
-        cv2.putText(img, f'Hand:{myHandType}', (bbox[0], bbox[1] - 30),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+        fingers1 = detector.fingersUp(hand1)
 
+        if len(hands) == 2:
+            # Hand 2
+            hand2 = hands[1]
+            lmList2 = hand2["lmList"]  # List of 21 Landmark points
+            bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
+            centerPoint2 = hand2['center']  # center of the hand cx,cy
+            handType2 = hand2["type"]  # Hand Type "Left" or "Right"
+
+            fingers2 = detector.fingersUp(hand2)
+
+            # Find Distance between two Landmarks. Could be same hand or different hands
+            length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)  # with draw
+            # length, info = detector.findDistance(lmList1[8], lmList2[8])  # with draw
     # Display
     cv2.imshow("Image", img)
     cv2.waitKey(1)
