e5cb54296ab13a89044848977f4b9a29479aa63e
AhmedAhmedEG <ahmed.ahmed.a.m.aa12@gmail.com>
2022-05-04 02:09:33 +0200

    Update HandTrackingModule.py

--- cvzone/HandTrackingModule.py
+++ cvzone/HandTrackingModule.py
@@ -155,8 +155,8 @@ class HandDetector:
                  Line information
         """
 
-        x1, y1 = p1
-        x2, y2 = p2
+        x1, y1, _ = p1
+        x2, y2, _ = p2
         cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
         length = math.hypot(x2 - x1, y2 - y1)
         info = (x1, y1, x2, y2, cx, cy)
