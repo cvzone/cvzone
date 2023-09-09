8e468e539f577efad7eaab53f558ef5d3822f73e
Murtaza Hassan <mhpr@live.co.uk>
2023-09-09 15:15:08 +0500

    removed mediapipe dependency for default installation

--- cvzone/__init__.py
+++ cvzone/__init__.py
@@ -1,6 +1,2 @@
 from cvzone.Utils import stackImages, cornerRect, findContours,\
     overlayPNG, rotateImage, putTextRect,downloadImageFromUrl
-from cvzone.ColorModule import ColorFinder
-from cvzone.FPS import FPS
-from cvzone.PIDModule import PID
-from cvzone.PlotModule import LivePlot
